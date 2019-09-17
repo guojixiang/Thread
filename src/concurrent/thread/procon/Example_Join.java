80c38c22d9e677b3c580afd038f8e8ea2090a7c8
guojixiang <lonewolf365@outlook.com>
2019-09-17 11:50:37 +0800

    线程间通信示例：join使用,管道字节流与字符流

---
+++ src/concurrent/thread/procon/Example_Join.java
@@ -0,0 +1,31 @@
+package concurrent.thread.procon;
+
+/**
+ * join 实现线程间通信
+ */
+public class Example_Join {
+
+    private static int a, b = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread_1 = new Thread(() -> {
+            a = 2;
+            System.out.println("Now, a is " + a);
+        });
+
+        Thread thread_2 = new Thread(() -> {
+            b = 2;
+            System.out.println("Now, b is " + b);
+        });
+
+        thread_1.start();
+        thread_2.start();
+        thread_1.join();
+        thread_2.join();
+
+        System.out.println("a+b should be 4, now is " + (a + b));
+
+    }
+
+
+}
